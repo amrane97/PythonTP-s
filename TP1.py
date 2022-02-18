d6be739895d4fe99e6829455f85239bfa8a176c2
amrane97 <djebarri.amrane@gmail.com>
2022-02-18 15:32:48 +0100

    syracuse

---
+++ TP1.py
@@ -0,0 +1,16 @@
+
+def sycaruse(n):
+
+    while (n != 1):
+        if (n % 2 == 0 and n > 0):
+            n = n//2
+            print(n)
+        elif (n % 2 == 1 and n > 0):
+            n = n*3 + 1
+            print(n)
+
+    return n
+
+
+print('veuillez insere un nombre entier positif : ')
+f = sycaruse(int(input()))
