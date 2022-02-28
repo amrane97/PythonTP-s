5623a2784751be3f9ca086597682657b2fef8b04
amrane97 <djebarri.amrane@gmail.com>
2022-02-28 22:11:13 +0100

    syracuse+mul_egyptienne

--- TP1.py
+++ TP1.py
@@ -1,4 +1,7 @@
 
+from os import remove
+
+
 def sycaruse(n):
 
     while (n != 1):
@@ -6,11 +9,25 @@ def sycaruse(n):
             n = n//2
             print(n)
         elif (n % 2 == 1 and n > 0):
+
             n = n*3 + 1
             print(n)
 
     return n
 
+def mul_egyptienne(a,b):
+    
+    s = 0
+    while a != 0:
+        if a % 2 == 1:
+            s += b
+        a = a // 2
+        b +=  b
 
-print('veuillez insere un nombre entier positif : ')
-f = sycaruse(int(input()))
+    print(s)
+
+print ("Veuillez inserez deux chiffres : ")
+a = int(input("chiffre1 : "))
+b = int(input("chiffre 2 : "))
+mul_egyptienne(a, b)
+
